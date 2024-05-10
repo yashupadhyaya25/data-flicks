22f48af833e0919ec48dd4065695a9f2810968de
Yash Upadhyaya <yashupadhyaya01@gmail.com>
2024-05-10 19:00:50 +0000

    md5 macro

--- macros/generate_md5.sql
+++ macros/generate_md5.sql
@@ -1,7 +1,7 @@
-{% macro group_generate_md5() -%}
-    {% set column_to_md5 = ''%}
-    {%- for key, value in kwargs.items() %}
-        {% set column_to_md5 = [column_to_md5]|{value} %}
-    {% endfor %}
-    select {{column_to_md5}} from {{ ref('stg_netflix__shows') }}
-{%- endmacro %}
+{%- macro group_generate_md5() -%}
+    {%- set column_to_md5 = [] -%}
+    {%- for key, value in kwargs.items() -%}
+        {%-do column_to_md5.append("NVL(CAST("~value~" as VARCHAR),'UNKNOW')") -%}      
+    {%- endfor -%}
+    {{'MD5(CONCAT(' ~ column_to_md5|join(",'_',") ~ '))'}}
+{%- endmacro -%}
